393073a9940cb112be6c793275e1fd0797a9ea74
Dorota Borowska <youcode2021db@gmail.com>
2022-03-29 20:53:33 +0200

    style-components added

---
+++ src/Component/Container/index.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container =styled.div`
+max-width: 1368px;
+margin: auto;
+background-color: ${({theme})=> theme.color.white}
+`;
+
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+    color: {
+        silver: "#ccc",
+        alto: "#ddd",
+        gallery: "#eee",
+        white: "#FFFFFF",
+        teal: "#008080",
+        cardinal: "#c41e3a",
+        forestGreen: "#228c22",
+        mystic: "#E4E6F0",
+        whisper: "#F5F5FA",
+        black: "#000000",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
