7a400091607150e58def3dfd5ef361c2091e25a0
Dorota Borowska <youcode2021db@gmail.com>
2022-03-30 11:12:06 +0200

    colors in theme changed

--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,11 @@
 export const theme = {
     color: {
         candleelight: "#FCD420",
-        silver: "#ccc",
-        alto: "#ddd",
         gallery: "#eee",
         white: "#FFFFFF",
-        teal: "#008080",
-        cardinal: "#c41e3a",
-        forestGreen: "#228c22",
         mystic: "#E4E6F0",
         whisper: "#F5F5FA",
+        stormGray: "#74788B",
         black: "#000000",
     },
     breakpoint: {
